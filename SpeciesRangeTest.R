02f83c37d2aade8d7ee8d5438e2971f3bea9e3c6
neetu111 <nshah.0452@gmail.com>
2018-03-06 15:36:30 +0530

    test 2: convex hull polygon

--- SpeciesRangeTest.R
+++ SpeciesRangeTest.R
@@ -2,6 +2,11 @@ library(rgbif)
 library(spocc)
 library(ggplot2)
 library(spoccutils)
+library(sp)
+library(rgeos)
+library(maptools)
+library(raster)
+library(dismo)
 
 # To Visualize the Data
 occ.data <- occ(query = 'Accipiter striatus', from = 'gbif')			# getting occurance data of Accipiter striatus species from GBIF
@@ -13,3 +18,12 @@ tail(df.occ.data)							# to see the data
 
 # Plotting Data on Map 
 map_ggplot(occ.data)
+
+# Convex Hull Polygon
+mat.occ.data <- data.matrix(df.occ.data, rownames.force = NA)	# converting to matrix 
+mat.occ.hull.data <- mat.occ.data[,c("longitude","latitude")]  	# taking longitude and latitude of the data
+data(wrld_simpl)								# getting data of world map
+plot(wrld_simpl,col="light yellow")				# plot world map
+points(mat.occ.hull.data[,1],mat.occ.hull.data[,2],col="red",cex=0.5)		# plot data points on map
+occ.hull.data <- convHull(mat.occ.hull.data)			# making convex hull around points
+plot(occ.hull.data,col=rgb(0, 0, 125, max = 255, alpha = 20*255/100),add=T)		# plot convex hull polygon around data points
