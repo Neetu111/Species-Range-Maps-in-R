6246989a302d65adeedac03c87006032cb898754
neetu111 <nshah.0452@gmail.com>
2018-03-07 18:40:49 +0530

    comments added

--- SpeciesRangeTest.R
+++ SpeciesRangeTest.R
@@ -1,3 +1,10 @@
+# This script can be used for the following task
+# 1. Downloading the species occurrence data from GBIF 
+# 2. Displaying this data on the world map
+# 3. Clipping the convex hull polygon around the occurrence data point 
+
+############################################################################################################
+#libraries required for this script
 library(rgbif)
 library(spocc)
 library(ggplot2)
@@ -8,22 +15,22 @@ library(maptools)
 library(raster)
 library(dismo)
 
-# To Visualize the Data
-occ.data <- occ(query = 'Accipiter striatus', from = 'gbif')			# getting occurance data of Accipiter striatus species from GBIF
-occ.data$gbif						# to see the data
+#############################################################################################################
+# Visualize the Data
+occ.data <- occ(query = 'Accipiter striatus', from = 'gbif')
+occ.data$gbif						
 occ.data$gbif$metadata				# to see the parameters, query, time the call executed
-df.occ.data <- occ2df(occ.data)			# Creating a Data Frame of data
-head(df.occ.data)							# to see the data
-tail(df.occ.data)							# to see the data
+df.occ.data <- occ2df(occ.data)			
+head(df.occ.data)							
 
-# Plotting Data on Map 
+# Plot Occurrence Data on World Map 
 map_ggplot(occ.data)
 
-# Convex Hull Polygon
-mat.occ.data <- data.matrix(df.occ.data, rownames.force = NA)	# converting to matrix 
-mat.occ.hull.data <- mat.occ.data[,c("longitude","latitude")]  	# taking longitude and latitude of the data
-data(wrld_simpl)								# getting data of world map
-plot(wrld_simpl,col="light yellow")				# plot world map
-points(mat.occ.hull.data[,1],mat.occ.hull.data[,2],col="red",cex=0.5)		# plot data points on map
-occ.hull.data <- convHull(mat.occ.hull.data)			# making convex hull around points
-plot(occ.hull.data,col=rgb(0, 0, 125, max = 255, alpha = 20*255/100),add=T)		# plot convex hull polygon around data points
+# Clip Convex Hull Polygon Around the Data Point
+df.occ.hull.data <- cbind.data.frame(df.occ.data$longitude, df.occ.data$latitude)	
+mat.occ.hull.data <- as.matrix.data.frame(df.occ.hull.data, rownames.force = NA)
+data(wrld_simpl)								
+plot(wrld_simpl,col="light yellow")				
+points(mat.occ.hull.data[,1],mat.occ.hull.data[,2],col="red",cex=0.5)		
+occ.hull.data <- convHull(mat.occ.hull.data)			
+plot(occ.hull.data,col=rgb(0, 0, 125, max = 255, alpha = 20*255/100),add=T)	
--- function.R
+++ function.R
@@ -1,33 +1,53 @@
-# Libraries needed to use this function
-# library(spocc)
-# library(ggplot2)
-# library(spoccutils)
-# library(sp)
-# library(rgeos)
-# library(maptools)
-# library(raster)
-# library(dismo)
+# This function displays the species occurrence data on the map and can create convex hull a around this data
+# For using this script following argument required
+# spc <- Species Name
+# src <- Which source you are using to get the data, i.e. GBIF
+# Map <- if one want to displays the data on map set TRUE, otherwise FALSE
+# cnvxHullMap <- if one want to displays the convex hull polygon around the data on map set TRUE, otherwise FALSE
 
-SpeciesRangeMap <- function(SpeciesName, SourceName, DataOnMap = TRUE, ConvexHullOfData = FALSE){
-	if(missing(SpeciesName)) stop("Because you did not specify species Name")
-	if(missing(SourceName)) stop("Because you did not specify source name")
-	occ.data <- occ(query = SpeciesName, from = SourceName)
+
+
+##########################################################################################################
+# Libraries needed to use this function
+library(spocc)
+library(ggplot2)
+library(spoccutils)
+library(sp)
+library(rgeos)
+library(maptools)
+library(raster)
+library(dismo)
+
+
+SpcRngMap <- function(spc, src, Map = TRUE, cnvxHullMap = FALSE){
+
+	#conditions to run the function
+	if(missing(spc)) stop("Because you did not specify species Name")
+	if(missing(src)) stop("Because you did not specify source name")
+	occ.data <- occ(query = spc, from = src)
 	df.occ.data <- occ2df(occ.data)
 	if(!nrow(df.occ.data)) stop("Because this species has no data on chosen source")
-	#mat.occ.data <- data.matrix(df.occ.data, rownames.force = NA)	# converting to matrix
-	mat.occ.data <- as.matrix.data.frame(df.occ.data, rownames.force = NA)	# converting to matrix 
-	mat.occ.hull.data <- mat.occ.data[,c("longitude","latitude")]  	# taking longitude and latitude of the data
-	data(wrld_simpl)								# getting data of world map
-	if(DataOnMap){
-		plot(wrld_simpl,col="light yellow")				# plot world map
-		points(mat.occ.hull.data[,1],mat.occ.hull.data[,2],col="red",cex=0.5)		# plot data points on map
+	
+	
+	df.occ.hull.data <- cbind.data.frame(df.occ.data$longitude, df.occ.data$latitude)	
+	mat.occ.hull.data <- as.matrix.data.frame(df.occ.hull.data, rownames.force = NA)
+	data(wrld_simpl)								
+	
+	# plot data points on map on world map
+	if((Map | !cnvxHullMap | missing(cnvxHullMap)) & !missing(Map)){
+		plot(wrld_simpl,col="light yellow")				
+		points(mat.occ.hull.data[,1],mat.occ.hull.data[,2],col="red",cex=0.5)		
 	}
-	if(ConvexHullOfData){
-		plot(wrld_simpl,col="light yellow")				# plot world map
-		points(mat.occ.hull.data[,1],mat.occ.hull.data[,2],col="red",cex=0.5)		# plot data points on map
-		occ.hull.data <- convHull(mat.occ.hull.data)			# making convex hull around points
-		plot(occ.hull.data,col=rgb(0, 0, 125, max = 255, alpha = 20*255/100),add=T)		# plot convex hull polygon around data points
+	
+	# plot convex hull around the data points on world map
+	if((cnvxHullMap| !Map | missing(Map)) & !missing(cnvxHullMap)){
+		plot(wrld_simpl,col="light yellow")				
+		points(mat.occ.hull.data[,1],mat.occ.hull.data[,2],col="red",cex=0.5)		
+		occ.hull.data <- convHull(mat.occ.hull.data)			
+		plot(occ.hull.data,col=rgb(0, 0, 125, max = 255, alpha = 20*255/100),add=T)	
 	}
 }
 
-SpeciesRangeMap(SpeciesName = 'Accipiter striatus', SourceName = 'gbif', DataOnMap = TRUE, ConvexHullOfData = FALSE)		# Test Case
+#############################################################################################################
+# Test Case
+SpcRngMap(spc = 'Accipiter striatus', src = 'gbif', Map = TRUE, cnvxHullMap = FALSE)		
