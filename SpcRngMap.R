5f436a5b01b1d67320d8b37f8982e67ff07e7a37
neetu111 <nshah.0452@gmail.com>
2018-03-10 04:23:55 +0530

    Convex Hull Polygon Clipped

--- SpcRngMap.R
+++ SpcRngMap.R
@@ -1,19 +1,22 @@
 # This script can be used for the following task
 # 1. Downloading the species occurrence data from GBIF 
 # 2. Displaying this data on the world map
-# 3. Clipping the convex hull polygon around the occurrence data point 
+# 3. Making convex hull polygon around the occurrence data point 
+# 4. Clipping the convex hull 
 
 ############################################################################################################
 #libraries required for this script
 library(rgbif)
 library(spocc)
 library(ggplot2)
+library(maps)
 library(spoccutils)
 library(sp)
 library(rgeos)
 library(maptools)
 library(raster)
 library(dismo)
+library(alphahull)
 
 #############################################################################################################
 # Visualize the Data
@@ -26,11 +29,21 @@ head(df.occ.data)
 # Plot Occurrence Data on World Map 
 map_ggplot(occ.data)
 
-# Clip Convex Hull Polygon Around the Data Point
-df.occ.hull.data <- cbind.data.frame(df.occ.data$longitude, df.occ.data$latitude)	
+# Convex Hull Polygon Around the Data Point
+df.occ.hull.data <- cbind.data.frame(longitude = df.occ.data$longitude, latitude = df.occ.data$latitude)	
 mat.occ.hull.data <- as.matrix.data.frame(df.occ.hull.data, rownames.force = NA)
 data(wrld_simpl)								
 plot(wrld_simpl,col="light yellow")				
-points(mat.occ.hull.data[,1],mat.occ.hull.data[,2],col="red",cex=0.5)		
+points(mat.occ.hull.data[,1], mat.occ.hull.data[,2],col="red",cex=0.5)		
 occ.hull.data <- convHull(mat.occ.hull.data)			
 plot(occ.hull.data,col=rgb(0, 0, 125, max = 255, alpha = 20*255/100),add=T)	
+
+# Clip the polygon to world map
+df.occ.hull.data <- cbind.data.frame(longitude = df.occ.data$longitude, latitude = df.occ.data$latitude)	
+df.hull.land.data = subset(df.occ.hull.data, !is.na(longitude))
+df.hull.land.data = df.hull.land.data[!duplicated(paste(df.hull.land.data$longitude, df.hull.land.data$latitude)), ]
+mat.hull.land.data <- as.matrix.data.frame(df.hull.land.data, rownames.force = NA)
+hull.land.data = ashape(mat.hull.land.data, alpha = 5.5)
+plot(wrld_simpl, xlim=c( -135.3431, -49.60951), ylim=c(-30.94649, 58.36106), axes=TRUE, col="light yellow")
+points(mat.occ.hull.data[,1], mat.occ.hull.data[,2],col="red",cex=0.5)		
+plot(hull.land.data,col=rgb(0, 0, 125, max = 255, alpha = 80*255/100),add=T)	
